af425dc13768a8281ef820a0c66746e5af5647fb
Ali Mohamud <alimohamud66@gmail.com>
2020-02-20 13:26:07 +0100

    skeletonf files

--- cchat/excersice/kitchen.erl
+++ cchat/excersice/kitchen.erl
@@ -1,5 +1,5 @@
 -module(kitchen).
--export([fridge/1, store/2, start/1]).
+-export([fridge/1, store/2, take/2, skriv/2, start/1]).
 
 start(Foodlist) ->
     spawn(?MODULE, fridge, [Foodlist]).
@@ -9,9 +9,20 @@ fridge(Foodlist) ->
     %how does it know that its a list, why does the argument begin with upper case%
     receive
         {From, {store, Food}} ->
-            From ! {self(),is_stored},
+            From ! {self(), is_stored},
             fridge([Food|Foodlist]);
-        exit -> 
+        {From, {take, Food}} -> 
+            case lists:member(Food, Foodlist) of
+                true ->
+                    From ! {self(), is_taken},
+                    fridge(lists:delete(Food, Foodlist));
+                false -> 
+                    From ! {self(), not_found},
+                    fridge(Foodlist)
+            end;
+        {From, {skriv, Food}} -> 
+            From ! {self(), {Food}, skriv};
+        terminate ->
             ok
     end.
 
@@ -21,4 +32,15 @@ store(Pid, Food) ->
        {Pid, Msg} -> Msg
     end.
 
+take(Pid, Food) -> 
+    Pid ! {self(), {take, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+skriv(Pid, Food) ->
+    Pid ! {self(), {skriv, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
 

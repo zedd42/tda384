ab75fb248f75c1243b0761b8d37a060f0fa71d38
almoh <almoh@student.chalmers.se>
2020-02-24 16:32:30 +0100

    startat lite på server

--- cchat/server.erl
+++ cchat/server.erl
@@ -1,18 +1,10 @@
 -module(server).
--export([start/1,stop/1]).
+-export([start/1]).
+-record (s_status, {active_clients = [], channel_map = maps:new()}).
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
-    % - Spawn a new process which waits for a message, handles it, then loops infinitely
-    % - Register this process to ServerAtom
-    % - Return the process ID
-    not_implemented.
 
-% Stop the server process registered to the given name,
-% together with any other associated processes
-stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+    
+   genserver:start(ServerAtom, #s_status{}, fun() -> io:fwrite("Anonymous Function") end).

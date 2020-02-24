566100b8c09fd146b41c40655da7bdf12ca23e83
almoh <almoh@student.chalmers.se>
2020-02-24 17:01:27 +0100

    startat lite på server

--- cchat/server.erl
+++ cchat/server.erl
@@ -1,5 +1,5 @@
 -module(server).
--export([start/1]).
+-export([start/1, stop/1, server_handler/2]).
 -record (s_status, {active_clients = [], channel_map = maps:new()}).
 
 % Start a new server process with the given name
@@ -7,4 +7,11 @@
 start(ServerAtom) ->
 
     
-   genserver:start(ServerAtom, #s_status{}, fun() -> io:fwrite("Anonymous Function") end).
+   genserver:start(ServerAtom, #s_status{}, server_handler/2).
+   
+stop(ServerAtom) ->
+	genserver:request(ServerAtom,{stop,ServerAtom}),
+	ok.
+
+	
+server_handler(s_status, {})

20da522ff9c3d680ea2d2a10107efb628b89b793
almoh <almoh@student.chalmers.se>
2020-02-25 22:49:23 +0100

    submitted version

--- cchat/client.erl
+++ cchat/client.erl
@@ -6,7 +6,8 @@
 -record(client_st, {
     gui, % atom of the GUI process
     nick, % nick/username of the client
-    server % atom of the chat server
+    server, % atom of the chat server
+    channel_map % joined channels  
 }).
 
 % Return an initial state record. This is called from GUI.
@@ -15,7 +16,8 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
     #client_st{
         gui = GUIAtom,
         nick = Nick,
-        server = ServerAtom
+        server = ServerAtom,
+        channel_map = maps:new() % joined channels  
     }.
 
 % handle/2 handles each kind of request from GUI
@@ -28,21 +30,103 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 
 % Join channel
 handle(St, {join, Channel}) ->
+
+      try
+      
+    _channel = list_to_atom(Channel),
+    _server = St#client_st.server,
+    _ref = make_ref(),
+    _myPid = self(),    
+    _server ! {request, _myPid, _ref, {req_channel, _channel, _myPid}},    
+    receive 
+      {result, _ref, Msg} ->
+	case Msg of 
+	 {user_added_to_channel_successfully, Channel_pid} -> 
+	  % update the map
+	    _newmap = maps:put(_channel, Channel_pid, St#client_st.channel_map),
+	    _newState = St#client_st{channel_map = _newmap},
+	    {reply, ok, _newState};
+	 {alredy_member_error} ->
+	    {reply, {error, user_already_joined, "You have joined earlier"}, St}
+	 end
+    end
+    catch
+                error:badarg -> {reply, {error, server_not_reached, "Channel unresponsive"}, St}
+    end;
+    
+    
+handle(St, {leave, Channel}) ->   
+    _channel = list_to_atom(Channel),
+    _channelMap = St#client_st.channel_map,
+    
+    case maps:is_key(_channel,_channelMap) of 
+       
+       true -> 
+	try
+	  _channelPid = maps:get(_channel,_channelMap),
+	  _ref = make_ref(),
+	  _myPid = self(),
+	  _channelPid ! {request,_myPid,_ref,{leave_channel_req,_myPid}},
+	   receive
+	    {result,_ref,Msg} ->
+	      case Msg of
+		user_leave_successfully ->
+		  {reply,ok,St};
+		user_not_member_error ->
+		  {reply,{error,error_user_not_joined,"Instruction failed: User not member of channel"},St}
+	      end
+	    
+	  end
+	    catch 
+	    error:badarg -> {reply, {error, server_not_reached, "Channel unresponsive"}, St}
+	  end;
+	     
+	false -> 
+	  {reply,{error,error_user_not_joined,"Instruction failed: User not member of channel"},St}
+	
+      end;
+	      
+		
+	      
+	  
+
+
+  
+
     % TODO: Implement this function
     % {reply, ok, St} ;
-    {reply, {error, not_implemented, "join not implemented"}, St} ;
-
-% Leave channel
-handle(St, {leave, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "leave not implemented"}, St} ;
-
+    
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "message sending not implemented"}, St} ;
+    
+   try
+    _channelMap = St#client_st.channel_map,
+    _channel = list_to_atom(Channel),  
+    _channelPid = maps:get(_channel,_channelMap),
+    _ref = make_ref(),
+    _myPid = self(),
+    _userNick = St#client_st.nick,
+    _channelPid ! {request,_myPid,_ref,{deliver_message,Msg,_userNick,_myPid}},
+      receive 
+	 {result,_ref, _response} ->
+	case _response of
+	  send_message_successfully ->
+	    {reply,ok,St};
+	  message_empty_error ->
+	    {reply,{error,message_empty_error,"Operation failed: Message empy"}, St}
+	   
+	 
+	end
+	
+      end
+      
+      catch
+      
+      error:badarg -> {reply, {error, server_not_reached, "Channel unresponsive"}, St}
+     
+     end;
+    
+   
 
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
@@ -50,7 +134,7 @@ handle(St, {nick, NewNick}) ->
     {reply, ok, St#client_st{nick = NewNick}} ;
 
 % ---------------------------------------------------------------------------
-% The cases below do not need to be changed...
+% The cases below do not need to be changed...flu
 % But you should understand how they work!
 
 % Get current nick
@@ -59,7 +143,7 @@ handle(St, whoami) ->
 
 % Incoming message (from channel, to GUI)
 handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
-    gen_server:call(GUI, {message_receive, Channel, Nick++"> "++Msg}),
+    gen_server:call(GUI, {message_receive, atom_to_list(Channel), Nick++"> "++Msg}),
     {reply, ok, St} ;
 
 % Quit client via GUI
--- cchat/server.erl
+++ cchat/server.erl
@@ -1,17 +1,120 @@
 -module(server).
 -export([start/1, stop/1, server_handler/2]).
--record (s_status, {active_clients = [], channel_map = maps:new()}).
+-record (server, {active_clients = [], channel_map = maps:new()}).
+-record (clients_connected_to_channel, {clients_pids = [], channel_name}).
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
 
     
-   genserver:start(ServerAtom, #s_status{}, server_handler/2).
+   genserver:start(ServerAtom, #server{}, fun server_handler/2).
    
 stop(ServerAtom) ->
-	genserver:request(ServerAtom,{stop,ServerAtom}),
-	ok.
+    genserver:request(ServerAtom, #server{active_clients = []}, fun server_handler/2).
 
-	
-server_handler(s_status, {})
+    
+    % begär att få tillgång till en kanal.
+    % kolla om kanalen existerar sen tidigare
+    % om den existerar 
+    
+server_handler(State, {req_channel, Channel_name, From_pid }) ->
+  
+  case maps:is_key(Channel_name, State#server.channel_map) of 
+            
+            true -> 
+                Channel_pid = maps:get(Channel_name, State#server.channel_map),         
+                Channel_pid ! {request, self(), make_ref(),{join_channel_request, From_pid}},
+
+           
+               receive
+                    {result, _, Msg} -> 
+                        case Msg of 
+                            client_added_to_channel_successfully ->
+                                {reply,{user_added_to_channel_successfully , Channel_pid}, State};
+                            alredy_member_error ->
+                                {reply, user_already_member_error, State}
+                        end
+                    end;
+            
+         
+               false -> 
+                    _new_ChannelPid = genserver:start(
+                        Channel_name, 
+                        #clients_connected_to_channel{clients_pids = [From_pid],channel_name = Channel_name }, 
+                        fun server_handler/2 ),
+                    _new_map = maps:put (Channel_name, _new_ChannelPid, State#server.channel_map),
+                    _newState = State#server{channel_map = _new_map},
+                    {reply, {user_added_to_channel_successfully, _new_ChannelPid }, _newState}
+       end  ;    
+                 
+
+server_handler(State, {leave_channel_req, UserPid}) -> 
+
+        case lists:member(UserPid, State#clients_connected_to_channel.clients_pids ) of 
+            true ->
+                _remainingClients = lists:delete(UserPid, State#clients_connected_to_channel.clients_pids),
+                _newState = State#clients_connected_to_channel{clients_pids = _remainingClients},
+                {reply, user_leave_successfully, _newState};
+            false -> 
+                {reply, user_not_member_error, State }
+        end;
+                
+            
+
+        
+        
+        
+        
+      
+server_handler(State, {join_channel_request, From_pid}) -> 
+
+        case lists:member(From_pid, State#clients_connected_to_channel.clients_pids) of
+            true ->
+                {reply, alredy_member_error, State};
+            false ->
+                _newState = State#clients_connected_to_channel{
+                        clients_pids = [From_pid |State#clients_connected_to_channel.clients_pids] },
+                   
+                {reply, client_added_to_channel_successfully, _newState}
+          end;
+
+          
+ server_handler(State, {deliver_message, Msg, _userNick, _fromPid}) ->
+    
+    if 
+        Msg == [] ->
+            {reply,message_empty_error,State};
+            
+        
+        true ->
+            spawn(fun() -> sendMessage(State,Msg,_userNick,_fromPid) end),
+            {reply,send_message_successfully,State}
+     end.
+        
+    
+
+%Elem!{request,self(),make_ref(),{message_receive,_channelName, _userNick, Msg}} 
+ 
+
+    
+
+        
+        
+
+sendMessage(State ,Msg, _userNick, _senderPid) ->
+
+        UserPids =     lists:delete(_senderPid,State#clients_connected_to_channel.clients_pids),
+        ChannelName = State#clients_connected_to_channel.channel_name,
+
+        [spawn(fun() -> genserver:request(Client, 
+                {message_receive, ChannelName, _userNick, Msg}) end) || Client <- UserPids].
+
+
+
+
+
+ 
+                    
+              
+        
